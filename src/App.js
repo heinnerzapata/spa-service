7b289ab37c6f5680c077e9b647baf57481db9a59
Heinner Zapata <heinnerzapata1991@gmail.com>
2020-04-04 22:36:06 -0400

    test deploy 2

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React HZ
+          Learn React HZ 2
         </a>
       </header>
     </div>
