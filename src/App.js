324680b84581275590da707aabb60d801dc10e63
Heinner Zapata <heinnerzapata1991@gmail.com>
2020-04-04 23:09:36 -0400

    test last

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React HZ 6
+          Learn React HZ 7
         </a>
       </header>
     </div>
