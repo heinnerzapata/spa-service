ce4cbca27e6bdb153b77d1fc0d811e6bc26102a1
Heinner Zapata <heinnerzapata1991@gmail.com>
2020-04-07 20:01:11 -0400

    simple change

--- src/pages/about/About.js
+++ src/pages/about/About.js
@@ -10,7 +10,7 @@ class About extends Component {
       <Session>
         <PageTitle title={this.props.t('pages.about.title')}></PageTitle>
         <PageContainer isMarginTopActivated={false}>
-          About Content HZ
+          About Content HZ 2
         </PageContainer>
       </Session>
     );
