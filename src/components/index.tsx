f19f85e6f4b9b43a2c1d381b9de0c29c6ac896d2
heinner zapata <heinnerzapata1991@gmail.com>
2020-11-10 22:28:05 -0500

    [CMMS-24] blink menu options header

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -6,6 +6,7 @@ import V7Chip from "components/v7Chip";
 import V7Preloader from "components/v7Preloader";
 import V7Footer from "components/v7Footer";
 import V7Header from "components/v7Header";
+import V7Menu from "components/v7Menu";
 import V7Avatar, { AVATAR_SIZES } from "components/v7Avatar";
 import V7Icon from "components/v7Icon";
 import V7LanguageSelector from "components/v7LanguageSelector";
@@ -34,5 +35,6 @@ export {
   AVATAR_SIZES,
   MENU_OPTIONS_TYPE,
   V7Alert,
+  V7Menu,
   ALERT_TYPES,
 };
